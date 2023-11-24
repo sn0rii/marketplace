c876c9be028df276e91ceeaa7024345c04aa84de
Wojtek M <ojciecneo@gmail.com>
2023-11-24 08:53:48 +0100

    admin dashboards done ( payload setup + nodemon )

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,15 +1,17 @@
 import dotenv from "dotenv";
+
 import path from "path";
+import payload from "payload";
 import type { InitOptions } from "payload/config";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 });
 
-let cashed = (global as any).payload;
+let cached = (global as any).payload;
 
-if (!cashed) {
-  cashed = (global as any).payload = {
+if (!cached) {
+  cached = (global as any).payload = {
     client: null,
     promise: null,
   };
@@ -23,4 +25,25 @@ export const getPayloadClient = async ({ initOptions }: Args = {}) => {
   if (!process.env.PAYLOAD_SECRET) {
     throw new Error("PAYLOAD_SECRET is missing");
   }
+
+  if (cached.client) {
+    return cached.client;
+  }
+
+  if (!cached.promise) {
+    cached.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    });
+  }
+
+  try {
+    cached.client = await cached.promise;
+  } catch (e: unknown) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.client;
 };
