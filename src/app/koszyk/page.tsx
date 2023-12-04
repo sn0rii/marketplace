d2b6d8b61b98faa77433f38dc2fb1ad429ba3830
Wojtek Mitan <wojtekmitan91@gmail.com>
2023-12-04 18:26:01 +0100

    cart part 2

--- src/app/koszyk/page.tsx
+++ src/app/koszyk/page.tsx
@@ -127,6 +127,16 @@ const Page = () => {
                 })}
             </ul>
           </div>
+          <section className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-7 lg:mt-0 lg:p-8">
+            <h2 className="text-lg font-medium text-gray-600">
+              Podsumowanie zamowienia
+            </h2>
+            <div className="mt-6 space-y-4">
+              <div className="flex items-center justify-between">
+                <p className=""></p>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </div>
