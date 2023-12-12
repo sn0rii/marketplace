e904e7b350a9d8ed12bc14489c12901751f9aefc
Wojtek M <ojciecneo@gmail.com>
2023-12-12 09:59:41 +0100

    next img problem sorted

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-        pathname: "**",
-        port: "3000",
-        protocol: "http",
-      },
+    domains: [
+      "localhost",
+      "https://bazarek.vercel.app/",
+      "https://voytec-marketplace.up.railway.app/",
     ],
   },
 };
