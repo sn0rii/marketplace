297bda73bec9c2d263d04dc12f53f4ce817d5e07
Wojtek M <ojciecneo@gmail.com>
2023-12-12 10:06:34 +0100

    next config update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "localhost",
-      "https://bazarek.vercel.app/",
-      "https://voytec-marketplace.up.railway.app/",
-    ],
+    domains: ["localhost", "https://voytec-marketplace.up.railway.app/"],
   },
 };
 
