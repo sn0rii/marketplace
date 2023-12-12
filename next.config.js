ec0f8d79fb4b6cc729b9e4f9749f80cc441db61d
Wojtek M <ojciecneo@gmail.com>
2023-12-12 14:45:46 +0100

    ewsfddfg

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   images: {
     remotePatterns: [
       {
@@ -14,20 +12,6 @@ const nextConfig = {
       },
     ],
   },
-=======
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "voytec-marketplace.up.railway.app",
-			},
-		],
-	},
->>>>>>> 58b6b697c40a6edff8689f39262b93e759132680
 };
 
 module.exports = nextConfig;
